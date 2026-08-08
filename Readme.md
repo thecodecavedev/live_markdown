639babadd0fec72eea5e0e8173ca1765213e9397
The Code Cave Dev <melusi.somkhanda@gmail.com>
2026-08-09 00:12:07 +0200

    working project

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# live markdown
+**live markdown** is a markdown tool built as a fun project, it has only one purpose to preview the markdown you are writing (**_live_**)
+
+**Available features:**
+- Headers
+- Text bolding
+- text italicising
+- list
+- horizontal line
+- link
+
+
+**Note:** this **ReadMe** file was created in **live markdown**
--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 let markdown_area = document.getElementById("markdown-text-area")
 let preview_area = document.getElementById("preview")
 
+
+function parse_markdown(val){
+  let html = val;
+
+  html = html.replace(/^# (.*$)/gim, '<h1>$1</h1>')
+  html = html.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+  html = html.replace(/\*(.*?)\*/g, '<em>$1</em>')
+  html = html.replace(/_(.*?)_/g, '<em>$1</em>')
+  html = html.replace(/^- (.*$)/gm, '<li><ul>$1</ul></li>')
+  html = html.replace(/\n/g, '<br>')
+  html = html.replace(/---/g, '<hr>')
+  html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>')
+  return html.trim()
+}
+
 markdown_area.addEventListener(("input"), function(event){
-    preview_area.innerHTML = event.target.value
+    // preview_area.innerHTML = event.target.value
+    preview_area.innerHTML =parse_markdown(event.target.value)
 })
