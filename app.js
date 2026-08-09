fe756132b535b86d8c1c68e9fea1c01730a0f378
The Code Cave Dev <melusi.somkhanda@gmail.com>
2026-08-09 19:36:53 +0200

    added export to pdf functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+
 let markdown_area = document.getElementById("markdown-text-area")
 let preview_area = document.getElementById("preview")
+let export_button = document.getElementById("export-pdf")
+
 const placeholder_markdown = `# Welcome to Live Markdown
 
 Start typing to see your **markdown** rendered *instantly* on the right.
@@ -14,24 +17,28 @@ Start typing to see your **markdown** rendered *instantly* on the right.
 
 Try deleting this text and writing your own. Happy writing!`;
 
+function parse_markdown(val) {
+    let html = val;
 
-function parse_markdown(val){
-  let html = val;
-
-  html = html.replace(/^# (.*$)/gim, '<h1>$1</h1>')
-  html = html.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
-  html = html.replace(/\*(.*?)\*/g, '<em>$1</em>')
-  html = html.replace(/_(.*?)_/g, '<em>$1</em>')
-  html = html.replace(/^- (.*$)/gm, '<li><ul>$1</ul></li>')
-  html = html.replace(/\n/g, '<br>')
-  html = html.replace(/---/g, '<hr>')
-  html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>')
-  return html.trim()
+    html = html.replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    html = html.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+    html = html.replace(/\*(.*?)\*/g, '<em>$1</em>')
+    html = html.replace(/_(.*?)_/g, '<em>$1</em>')
+    html = html.replace(/^- (.*$)/gm, '<li><ul>$1</ul></li>')
+    html = html.replace(/\n/g, '<br>')
+    html = html.replace(/---/g, '<hr>')
+    html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>')
+    html = html.replace(/`(.*?)`/g, '<code>$1</code>')
+    return html.trim()
 }
 markdown_area.value = placeholder_markdown
 preview_area.innerHTML = parse_markdown(markdown_area.value)
-markdown_area.addEventListener(("input"), function(event){
+markdown_area.addEventListener(("input"), function (event) {
     // preview_area.innerHTML = event.target.value
-    preview_area.innerHTML =parse_markdown(event.target.value)
-    
+    preview_area.innerHTML = parse_markdown(event.target.value)
+
+})
+
+export_button.addEventListener(('click'), function (e) {
+    html2pdf().from(preview_area).save("livemarkdown.pdf")
 })
